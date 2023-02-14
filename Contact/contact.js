5d2cff1be4945b701acafa0d4932f38ca95fe7e2
mateociric <mciricprogramming@gmail.com>
2023-02-14 11:58:31 +0100

    moving validation from button to submit form

--- Contact/contact.js
+++ Contact/contact.js
@@ -9,15 +9,14 @@ let loginSurname = document.getElementById('loginSurname');
 let loginEmail = document.getElementById('loginEmail');
 let loginTextArea = document.getElementById('loginTextArea');
 let loginSubscribe = document.getElementById('loginSubscribe');
-let btnSubmit = document.getElementById('btnSubmit');
-let errors = 0;
 
 contactGuard(USER_DETAIL, window.location.origin);
 
-btnSubmit.addEventListener('click', () => {
-
+loginForm.addEventListener('submit', (e) => {
+    e.preventDefault();
+    
+    let errors = 0;
     let arrInput = getAllInput(loginForm);
-    errors = 0;
 
     arrInput.forEach((el) => {
         let keyInPattern = el.getAttribute('id');
@@ -40,10 +39,6 @@ btnSubmit.addEventListener('click', () => {
     USER_DETAIL.DETAILS = loginTextArea.value;
     loginSubscribe.checked ? USER_DETAIL.SUBSCRIBE = 'I want subscribe' : USER_DETAIL.SUBSCRIBE = 'I do not want to subscribe'
 
-});
-
-loginForm.addEventListener('submit', (e) => {
-    e.preventDefault();
     if (errors === 0) {
         localStorage.setItem('user', JSON.stringify(USER_DETAIL));
         window.location.assign(`${window.location.origin}/overview/overview.html`);
