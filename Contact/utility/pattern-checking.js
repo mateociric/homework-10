d8b4a44ef900f5919a798183807a5c6a2088c143
mateociric <mciricprogramming@gmail.com>
2023-02-14 13:55:26 +0100

    corr validator function

--- Contact/utility/pattern-checking.js
+++ Contact/utility/pattern-checking.js
@@ -1,59 +1,53 @@
 const pattern = {
     loginName: {
-        length: checkLenght,
-        minLenght: checkMinLenght,
-        maxLength: checkMaxLength,
+        zeroLength: checkZeroLenght,
+        minLenght: checkMinLenght(5),
+        maxLength: checkMaxLength(15),
     },
     loginSurname: {
-        length: checkLenght,
-        minLenght: checkMinLenght,
-        maxLength: checkMaxLength,
+        zeroLength: checkZeroLenght,
+        minLenght: checkMinLenght(5),
+        maxLength: checkMaxLength(15),
     },
     loginEmail: {
-        length: checkLenght,
-        minLenght: checkMinLenght,
-        maxLength: checkMaxLengthEmail,
+        zeroLength: checkZeroLenght,
+        minLenght: checkMinLenght(5),
+        maxLength: checkMaxLength(20),
         reg: checkRegEmail,
     },
     loginTextArea: {
-        length: checkLenght,
-        minLenght: checkMinLenght,
-        maxLength: checkMaxLengthTextarea,
+        zeroLength: checkZeroLenght,
+        minLenght: checkMinLenght(5),
+        maxLength: checkMaxLength(30),
     },
 }
 
-function checkLenght(input) {
+function checkZeroLenght(input) {
     input.nextElementSibling.textContent = '';
     if (!input.value.trim().length) {
         input.nextElementSibling.textContent = 'Invalid input!';
     }
 }
-function checkMinLenght(input) {
-    if (input.value.trim().length < 5 && input.nextElementSibling.textContent === '') {
-        input.nextElementSibling.textContent = 'Min is 5 chr!';
+function checkMinLenght(minLenght) {
+    return (input) => {
+        if (input.value.trim().length < minLenght && input.nextElementSibling.textContent === '') {
+            input.nextElementSibling.textContent = `Min is ${minLenght} chr!`;
+        }
     }
 }
-function checkMaxLength(input) {
-    if (input.value.trim().length > 16 && input.nextElementSibling.textContent === '') {
-        input.nextElementSibling.textContent = 'Max is 15 chr!';
+function checkMaxLength(maxLength) {
+    return (input) => {
+        if (input.value.trim().length > maxLength && input.nextElementSibling.textContent === '') {
+            input.nextElementSibling.textContent = `Max is ${maxLength} chr!`;
+        }
     }
 }
 function checkRegEmail(input) {
     let validateEmail = String(input.value).toLowerCase().match(
         /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
-    if(validateEmail === null && input.nextElementSibling.textContent === '') {
+    if (validateEmail === null && input.nextElementSibling.textContent === '') {
         input.nextElementSibling.textContent = 'Not valid email!';
-    }
-}
-function checkMaxLengthEmail(input) {
-    if (input.value.trim().length > 30 && input.nextElementSibling.textContent === '') {
-        input.nextElementSibling.textContent = 'Max is 15 chr!';
-    }
-}
-function checkMaxLengthTextarea(input) {
-    if (input.value.trim().length > 31 && input.nextElementSibling.textContent === '') {
-        input.nextElementSibling.textContent = 'Max is 30 chr!';
     }
 }
 
