19d7b56468010ec44ddf0f811a819f20dceced5d
mateociric <mciricprogramming@gmail.com>
2023-02-14 09:46:55 +0100

    corr. file name and USER_DETAIL obj

--- Contact/contact.js
+++ Contact/contact.js
@@ -1,4 +1,4 @@
-import USER_DETAILS from "../USER_DETAILS.js";
+import USER_DETAIL from "../user-detail.js";
 import getAllInput from "./add-on/input-collection.js";
 import pattern from "./add-on/pattern-checking.js";
 import { location } from "../location.js";
@@ -13,7 +13,7 @@ let loginSubscribe = document.getElementById('loginSubscribe');
 let btnSubmit = document.getElementById('btnSubmit');
 let errors = 0;
 
-contactGuard(USER_DETAILS, location);
+contactGuard(USER_DETAIL, location);
 
 btnSubmit.addEventListener('click', () => {
 
@@ -35,18 +35,18 @@ btnSubmit.addEventListener('click', () => {
 
     console.log(errors);
 
-    USER_DETAILS.NAME = loginName.value;
-    USER_DETAILS.SURNAME = loginSurname.value;
-    USER_DETAILS.EMAIL = loginEmail.value;
-    USER_DETAILS.DETAILS = loginTextArea.value;
-    loginSubscribe.checked ? USER_DETAILS.SUBSCRIBE = 'I want subscribe' : USER_DETAILS.SUBSCRIBE = 'I do not want to subscribe'
+    USER_DETAIL.NAME = loginName.value;
+    USER_DETAIL.SURNAME = loginSurname.value;
+    USER_DETAIL.EMAIL = loginEmail.value;
+    USER_DETAIL.DETAILS = loginTextArea.value;
+    loginSubscribe.checked ? USER_DETAIL.SUBSCRIBE = 'I want subscribe' : USER_DETAIL.SUBSCRIBE = 'I do not want to subscribe'
 
 });
 
 loginForm.addEventListener('submit', (e) => {
     e.preventDefault();
     if (errors === 0) {
-        localStorage.setItem('user', JSON.stringify(USER_DETAILS));
+        localStorage.setItem('user', JSON.stringify(USER_DETAIL));
         window.location.assign(`${location}//Overview/overview.html`);
     } else
         localStorage.clear();
---
+++ user-detail.js
@@ -0,0 +1,9 @@
+const USER_DETAIL = {
+    NAME: null,
+    SURNAME: null,
+    EMAIL: null,
+    DETAILS: null,
+    SUBSCRIBE: null,
+}
+
+export default USER_DETAIL;
